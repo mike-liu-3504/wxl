f1c32287f6b9a2227e6d5e100c85c25186399e8f
lhl <lhl@>
2022-06-26 15:20:05 +0800

    修复tomact启动报错问题

--- ssm/src/main/java/com/wangli/ssm/controller/UserController.java
+++ ssm/src/main/java/com/wangli/ssm/controller/UserController.java
@@ -1,27 +1,27 @@
-//package com.wangli.ssm.controller;
-//
-//import com.wangli.ssm.po.User;
-//import com.wangli.ssm.service.UserService;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import javax.annotation.Resource;
-//
-///**
-// * @description:
-// * @author: wl
-// * @create: 2022/6/23
-// **/
-//@Controller
-//public class UserController {
-//
-//    @Resource
-//    private UserService userService;
-//
-//    @RequestMapping("query")
-//    @ResponseBody
-//    public User queryUser(Integer userId){
-//        return userService.queryUser(userId);
-//    }
-//}
+package com.wangli.ssm.controller;
+
+import com.wangli.ssm.po.User;
+import com.wangli.ssm.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * @description:
+ * @author: wl
+ * @create: 2022/6/23
+ **/
+@Controller
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("query")
+    @ResponseBody
+    public User queryUser(Integer userId){
+        return userService.queryUserById(userId);
+    }
+}
